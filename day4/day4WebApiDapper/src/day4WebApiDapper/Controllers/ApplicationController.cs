bf7a01583d39cb3cf29a098c2c4d3f86828e842a
ShketovIgor350501 <igorshketov@yandex.ru>
2016-07-14 00:11:31 +0300

    day4 Web Api and Dapper

---
+++ day4/day4WebApiDapper/src/day4WebApiDapper/Controllers/ApplicationController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using day4WebApiDapper.Models;
+using Dapper;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace day4WebApiDapper.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApplicationController : Controller
+    {
+        private const string ConnectionStringName = "UsersGroups";
+
+        private readonly DataBaseConnectionProvider _connectionProvider;
+        private readonly string _connectionString;
+
+        public ApplicationController()
+        {
+            _connectionProvider = new DataBaseConnectionProvider();
+            _connectionString = Startup.ConnectionString;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Application> Get()
+        {
+            IEnumerable<Application> result;
+
+            using (var connection = _connectionProvider.GetConnection(_connectionString))
+            {
+                connection.Open();
+                result = connection.Query<Application>("SELECT Id, Name, UserId FROM Applications");
+                connection.Close();
+            }
+
+            return result;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ day4/day4WebApiDapper/src/day4WebApiDapper/DataBaseConnectionProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace day4WebApiDapper
+{
+    public class DataBaseConnectionProvider
+    {
+        public IDbConnection GetConnection(String connectionString)
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
