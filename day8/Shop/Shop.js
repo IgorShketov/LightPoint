2ddfd7cad9512eb912d29629ba8600471b65b8f9
ShketovIgor350501 <igorshketov@yandex.ru>
2016-07-19 19:39:01 +0300

    js module shop

---
+++ day8/Shop/Shop.js
@@ -0,0 +1,20 @@
+﻿shop = (function () {
+    var products = [];
+
+    return {
+        addNewProduct: function (productName, productPrice, productDescription) {
+            var product = { productName: productName, productPrice: productPrice, productDescription: productDescription };
+            products.push(product);
+        },
+
+        listProducts: function () {
+            products.forEach(function (item, i, arr) {
+                alert((i + 1) + ": " + item.productName + " " + item.productPrice + " " + item.productDescription);
+            });
+        },
+
+        deleteProduct: function (productNum) {
+            products.splice(productNum, 1);
+        }
+    }
+})();
