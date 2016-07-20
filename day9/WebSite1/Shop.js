e8fe93c28a3f9f1789a1416589327c42512f1d85
ShketovIgor350501 <igorshketov@yandex.ru>
2016-07-20 20:09:05 +0300

    day9 homework: js + html + css shop

---
+++ day9/WebSite1/Shop.js
@@ -0,0 +1,47 @@
+﻿shop = (function () {
+    var products = [];
+
+    function _addNewProduct (productName, productPrice, productDescription) {
+        var product = { productName: productName, productPrice: productPrice, productDescription: productDescription };
+        products.push(product);
+    };
+
+    function _getProducts () {
+        return products;
+    };
+
+    function _sortByPrice() {
+        products.sort(function(item1, item2) {
+            return item1.productPrice - item2.productPrice;
+        });
+    };
+
+    function _sortByName() {
+        products.sort(function(item1, item2) {
+            if (item1.productName > item2.productName) {
+                return 1;
+            }
+            if (item1.productName < item2.productName) {
+                return -1;
+            }
+            return 0;
+        });
+    };
+
+    function _deleteProduct (productNum) {
+        products.splice(productNum, 1);
+    };
+
+    function _getProductsAmount() {
+        return products.length;
+    };
+
+    return {
+        addNewProduct: _addNewProduct,
+        getProducts: _getProducts,
+        sortByName: _sortByName,
+        sortByPrice: _sortByPrice,
+        deleteProduct: _deleteProduct,
+        getProductsAmount: _getProductsAmount
+    }
+})();
