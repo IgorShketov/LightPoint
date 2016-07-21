1781e649307745bf574dbb328cec476406fd4235
ShketovIgor350501 <igorshketov@yandex.ru>
2016-07-21 18:03:35 +0300

    day 9 shop

--- day9/WebSite1/Shop.js
+++ day9/WebSite1/Shop.js
@@ -1,5 +1,8 @@
 ﻿shop = (function () {
-    var products = [];
+    var products = [],
+        nameSortDirection = 1,
+        priceSortDirection = 1;
+    
 
     function _addNewProduct (productName, productPrice, productDescription) {
         var product = { productName: productName, productPrice: productPrice, productDescription: productDescription };
@@ -10,22 +13,36 @@
         return products;
     };
 
+    function _getProduct(productNum) {
+        return products[productNum];
+    };
+
     function _sortByPrice() {
         products.sort(function(item1, item2) {
-            return item1.productPrice - item2.productPrice;
+            return (item1.productPrice - item2.productPrice) * priceSortDirection;
         });
+
+        priceSortDirection *= -1;
     };
 
     function _sortByName() {
         products.sort(function(item1, item2) {
             if (item1.productName > item2.productName) {
-                return 1;
+                return 1 * nameSortDirection;
             }
             if (item1.productName < item2.productName) {
-                return -1;
+                return -1 * nameSortDirection;
             }
             return 0;
         });
+
+        nameSortDirection *= -1;
+    };
+
+    function _changeProduct(productNum, _productName, _productPrice, _productDescription) {
+        products[productNum].productName = _productName;
+        products[productNum].productPrice = _productPrice;
+        products[productNum].productDescription = _productDescription;
     };
 
     function _deleteProduct (productNum) {
@@ -39,8 +56,10 @@
     return {
         addNewProduct: _addNewProduct,
         getProducts: _getProducts,
+        getProduct: _getProduct,
         sortByName: _sortByName,
         sortByPrice: _sortByPrice,
+        changeProduct: _changeProduct,
         deleteProduct: _deleteProduct,
         getProductsAmount: _getProductsAmount
     }
