863543aaa235edae9850d9f24321fd6978114f0d
ShketovIgor350501 <igorshketov@yandex.ru>
2016-07-24 19:57:52 +0300

    angular js homework read, delete

---
+++ day11/AngularHomework/angular/controllers/personsController.js
@@ -0,0 +1,25 @@
+﻿var personsApp = angular.module('personsApp');
+
+personsApp.controller('personsController', personsController);
+
+function personsController($scope, $http) {
+
+    $http.get('angular/models/persons.json').success(function (response) {
+        $scope.persons = response;
+    });
+
+    $scope.phoneSelect = "Home phone Number";
+
+    $scope.getPhone = function(person) {
+        if ($scope.phoneSelect == "Home phone Number") {
+            return person.phoneNumber[0].number;
+        }
+        else if ($scope.phoneSelect == "Fax") {
+            return person.phoneNumber[1].number;
+        }
+    }
+
+    $scope.removePerson = function (row) {
+        $scope.persons.splice($scope.persons.indexOf(row), 1);
+    };
+};
