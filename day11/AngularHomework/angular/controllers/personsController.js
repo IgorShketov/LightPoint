4cef2506c7cafb978fc61e2570f3605bfd94ded4
ShketovIgor350501 <igorshketov@yandex.ru>
2016-07-26 23:35:07 +0300

    angular homework

--- day11/AngularHomework/angular/controllers/personsController.js
+++ day11/AngularHomework/angular/controllers/personsController.js
@@ -2,13 +2,19 @@
 
 personsApp.controller('personsController', personsController);
 
-function personsController($scope, $http) {
+function personsController($scope, personsService, $http) {
 
-    $http.get('angular/models/persons.json').success(function (response) {
-        $scope.persons = response;
+    var promise = personsService.getPersons();
+    promise.then(function (resolve) {
+        $scope.persons = resolve;
     });
 
     $scope.phoneSelect = "Home phone Number";
+    $scope.deletedPersonIndex;
+
+    $scope.setDeletedPersonIndex = function (index) {
+        $scope.deletedPersonIndex = index;
+    }
 
     $scope.getPhone = function(person) {
         if ($scope.phoneSelect == "Home phone Number") {
@@ -19,7 +25,71 @@ function personsController($scope, $http) {
         }
     }
 
-    $scope.removePerson = function (row) {
-        $scope.persons.splice($scope.persons.indexOf(row), 1);
+    function clearFields(person) {
+        delete person._firstName;
+        delete person._lastName;
+        delete person._age;
+        delete person._address;
+        delete person._phoneNumber;
+    }
+
+    $scope.addNewPerson = function (person, form) {
+        if(form.$valid)
+        {
+            var newPerson = {};
+
+            newPerson.id = 1;
+            newPerson.firstName = person._firstName;
+            newPerson.lastName = person._lastName;
+            newPerson.age = person._age;
+            newPerson.address = person._address;
+            newPerson.phoneNumber = [];
+            newPerson.phoneNumber.push({ type: "home", number: person._phoneNumber[0].number });
+            newPerson.phoneNumber.push({ type: "fax", number: person._phoneNumber[1].number });
+
+            form.$setPristine();
+            form.$setUntouched();
+
+            $http.put('angular/models/persons.json', $scope.persons).then(function (data) {
+                console.log('done');
+            });
+
+            $scope.persons.push(newPerson);
+            $scope.dismiss();
+            clearFields(person);
+        }
+    }
+
+    $scope.removePerson = function (index) {
+        $scope.persons.splice(index, 1);
     };
 };
+
+personsApp.filter('meFilter', function () {
+    return function (persons, filterPattern) {
+        if (filterPattern === undefined) {
+            return persons;
+        }
+        else {
+            var personsFiltered = [];
+            persons.forEach(function (item) {
+                if ((item.lastName + ' ' + item.firstName).toLowerCase().indexOf(filterPattern) !== -1 ||
+                    (item.firstName + ' ' + item.lastName).toLowerCase().indexOf(filterPattern) !== -1) {
+                    personsFiltered.push(item);
+                }
+            });
+            return personsFiltered;
+        }
+    }
+});
+
+personsApp.directive('personModal', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            scope.dismiss = function () {
+                element.modal('hide');
+            };
+        }
+    }
+});
---
+++ day11/AngularHomework/angular/services/personsService.js
@@ -0,0 +1,14 @@
+﻿var personsApp = angular.module('personsApp');
+
+personsApp.factory('personsService', function ($http, $q) {
+
+    return {
+        getPersons: function () {
+            var deferred = $q.defer();
+            $http.get('angular/models/persons.json').success(function (response) {
+                deferred.resolve(response);
+            });
+            return deferred.promise;
+        }
+    }
+});
